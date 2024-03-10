4f3d810741c7fa422c0b41dc70ec4e1287e2f010
mateusisrael <mateuss.i.fernandes@gmail.com>
2024-03-09 23:22:09 -0300

    hello rust

---
+++ chapter_1/hello_cargo/target/debug/deps/hello_cargo-998b8934749f5499.d
@@ -0,0 +1,5 @@
+/home/mts/Documentos/dev/learning-rust/chapter_1/hello_cargo/target/debug/deps/hello_cargo-998b8934749f5499: src/main.rs
+
+/home/mts/Documentos/dev/learning-rust/chapter_1/hello_cargo/target/debug/deps/hello_cargo-998b8934749f5499.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/mts/Documentos/dev/learning-rust/chapter_1/hello_cargo/target/debug/hello_cargo: /home/mts/Documentos/dev/learning-rust/chapter_1/hello_cargo/src/main.rs
